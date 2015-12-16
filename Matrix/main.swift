df67a9d4003b792ceb96f68f693d9738eaa1339b
Matt Egan <matt.matt.egan@gmail.com>
2015-12-16 00:32:03 -0500

    [CNG] flushed out a bit more, operations are now instance methods, with the operators being sugar, added a transpose, multiply, and element-wise multiply and divide

--- Matrix/main.swift
+++ Matrix/main.swift
@@ -8,10 +8,15 @@
 
 import Foundation
 
-print(Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]).toString(0))
+
 print(Matrix.I(3))
 print(Matrix.diagonal([1, 2, 3, 4, 5, 6], pad: 2.5))
 var x = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 var y = Matrix([[20, 1, 5], [20, 1, 5], [20, 1, 5]])
+var z = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
 print(x + y)
-
+print(Matrix.I(3).transpose())
+print(x.transpose())
+print(z.transpose())
+print(x.multiply(y))
+print(y.multiply(x))
